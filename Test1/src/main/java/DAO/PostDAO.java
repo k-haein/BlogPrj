170b3e8663d0c44a9cb163744188bb8cd00feaaa
김해인 <k.haein96@gmail.com>
2022-11-11 18:47:00 +0900

    미리보기 내용을 따로 가져와서 붙여넣어야할 것 같음. style태그가 잘리지 않음.

--- Test1/src/main/java/DAO/PostDAO.java
+++ Test1/src/main/java/DAO/PostDAO.java
@@ -214,14 +214,14 @@ public class PostDAO {
 						
 						//--- 게시글 내용 가져와서 15자만 미리보기로 보여줌 ---
 						String preStr=rs.getString("POST_CONTENT");
-						System.out.println(preStr);
-						String cpreStr=preStr.replaceAll("<(/)?([a-zA-Z]*)(\\\\s[a-zA-Z]*=[^>]*)?(\\\\s)*(/)?>", "");
-						System.out.println("변경후 왜 안잘려"+cpreStr);
-				
-						if(preStr.length()>30){
-							
-							preStr=preStr.substring(0,15)+"..."; //자르고 ... 붙이기
-						};
+//						System.out.println(preStr);
+//						String cpreStr=preStr.replaceAll("<(/)?([a-zA-Z]*)(\\\\s[a-zA-Z]*=[^>]*)?(\\\\s)*(/)?>", "");
+//						System.out.println("변경후 왜 안잘려"+cpreStr);
+//				
+//						if(preStr.length()>30){
+//							
+//							preStr=preStr.substring(0,15)+"..."; //자르고 ... 붙이기
+//						};
 						pb.setPOST_CONTENT(preStr); //게시글내용 미리보기
 						//----------------------------------------
 						
