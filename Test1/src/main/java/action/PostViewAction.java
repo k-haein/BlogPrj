e99259d7e58dbe1070e49e4913a12968b531d154
김해인 <k.haein96@gmail.com>
2022-11-28 18:13:17 +0900

    블로그 프로젝트

--- Test1/src/main/java/action/PostViewAction.java
+++ Test1/src/main/java/action/PostViewAction.java
@@ -27,6 +27,8 @@ public class PostViewAction implements Action {
 			PostViewService postViewService = new PostViewService();
 			//서비스 svc에서 만들어준다. 아래에 게시글상세정보 표시를 해주자.
 			PostBean postObj = postViewService.getPostinfo(ClickPostNo);
+			
+			//서비스만 하나 더 만들어서 동일한 Action 페이지에 뿌려줄 예정.
 
 			req.setAttribute("postObj", postObj);
 
